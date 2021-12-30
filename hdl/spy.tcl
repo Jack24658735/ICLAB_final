91a6917cc421ca5b3ec2858e6c0a6b9710abfca5
leothebestcoder <leoshieh0hn@gmail.com>
2021-12-30 15:24:26 +0800

    add spyglass tcl file

---
+++ hdl/spy.tcl
@@ -0,0 +1,31 @@
+# read file
+read_file -type verilog top.v
+
+
+# goal setup (lint_rtl)
+current_goal lint/lint_rtl -alltop
+
+# run goal 
+run_goal
+
+# create a report file and write report message to the file
+capture spyglass_final_project.rpt {write_report moresimple}
+
+# setup another goal
+current_goal lint/lint_turbo_rtl -alltop
+
+# run goal 
+run_goal 
+
+# append the report message to the same file created above 
+capture -append spyglass_final_project.rpt {write_report moresimple}
+
+
+
+current_goal lint/lint_functional_rtl -alltop
+run_goal
+capture -append spyglass_final_project.rpt {write_report moresimple}
+
+current_goal lint/lint_abstract -alltop
+run_goal
+capture -append spyglass_final_project.rpt {write_report moresimple}
