a62e34baf887d049df220e9af7489ebff4b5b346
Jack24658735 <liujack0914@gmail.com>
2021-12-30 22:25:15 +0800

    Add denoise.v and med of med

--- hdl/median.v
+++ hdl/median.v
@@ -1,40 +1,38 @@
 module median
-#(
-
-)
 (
     input clk,
+    input rst_n,
     input [8-1:0] val_0,
     input [8-1:0] val_1,
     input [8-1:0] val_2,
     output reg [8-1:0] med
 );
 
-reg [8-1:0] val_0_r;
-reg [8-1:0] val_1_r;
-reg [8-1:0] val_2_r;
 reg [8-1:0] med_n;
 
 always @(*) begin
     // case1: val_0 is maximum
-    if (val_0_r >= val_1_r && val_0_r >= val_2_r) begin
-        med_n = (val_1_r >= val_2_r) ? val_1_r : val_2_r;
+    if (val_0 >= val_1 && val_0 >= val_2) begin
+        med_n = (val_1 >= val_2) ? val_1 : val_2;
     end
     // case2: val_1 is maximum
-    else if (val_1_r >= val_0_r && val_1_r >= val_2_r) begin
-        med_n = (val_0_r >= val_2_r) ? val_0_r : val_2_r;
+    else if (val_1 >= val_0 && val_1 >= val_2) begin
+        med_n = (val_0 >= val_2) ? val_0 : val_2;
     end
     // case3: val_2 is maximum
     else begin
-        med_n = (val_0_r >= val_1_r) ? val_0_r : val_1_r;
+        med_n = (val_0 >= val_1) ? val_0 : val_1;
     end
 end
 
 always @(posedge clk) begin
-    val_0_r <= val_0;
-    val_1_r <= val_1;
-    val_2_r <= val_2;
-    med <= med_n;
+    if (~rst_n) begin
+        med <= 0;
+    end
+    else begin
+        med <= med_n;
+    end
+    
 end
 endmodule
 
